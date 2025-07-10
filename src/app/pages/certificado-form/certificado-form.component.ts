0115dd982ec31a413e66be362f3810ea1d2149ac
Pedro Vieira <pedroalbuquerque4455@gmail.com>
2025-07-09 21:02:31 -0300

    armazenando certificados

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -4,6 +4,7 @@ import { PrimaryButtonComponent } from "../../_components/primary-button/primary
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Certificado } from '../../interfaces/CertificadoInterface';
+import { CertificadoService } from '../../services/certificado.service';
 
 @Component({
   selector: 'app-certificado-form',
@@ -15,8 +16,11 @@ export class CertificadoFormComponent {
   atividade: string = ''
   certificado: Certificado = {
     nome: '',
-    atividades: []
+    atividades: [],
+    data: ''
   }
+
+  constructor(private certificadoService: CertificadoService){}
 
   formValid(){
     return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0
@@ -33,7 +37,21 @@ export class CertificadoFormComponent {
 
   submit(){
     if(this.formValid()){
-      console.log(this.certificado)
+
+      this.certificado.data = this.formatarData()
+      this.certificadoService.adicionarCertificado(this.certificado)
     }
+  }
+
+  formatarData(){
+    const dataAtual = new Date()
+
+    const dia = dataAtual.getDate()
+    const mes = dataAtual.getMonth()+1
+    const ano = dataAtual.getFullYear()
+
+    const dataFormatada = `${dia}/${mes}/${ano}`
+
+    return dataFormatada
   }
 }
